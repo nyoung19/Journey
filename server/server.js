8d6cf9988d66d2845f8f48ff580c6d5b5aa49ff2
Nicole Young <nyoung7501@gmail.com>
2023-06-26 17:27:28 -0400

    creating new branch

--- server/server.js
+++ server/server.js
@@ -3,15 +3,28 @@ const app = express(); //APP IS AN INSTANCE OF EXPRESS
 const PORT = 3000;
 const path = require('path');
 
+
 //MIDDLEWARE FOR PARSING JSON REQUEST BODIES
 // app.use(express.json()); 
 
-app.use(express.static(path.resolve(__dirname, '../client')));
-//ISSUES WITH PROPERLY SERVING INDEX HTML - MIME TYPE ISSUE
-app.get('/', (req, res) => {
-  res.status(200).send("hello world");
-  // express.static(path.resolve(__dirname, '../index.html'));
-});
+// app.use(express.static('public')); //works, why doesnt it work with path.resolve (below), isnt it better to have path for absolute routing or whatever (below)
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname,'../public/index.html'));
+// }); 
+
+// app.get('/index.js', (req, res) => {
+//   res.sendFile(path.resolve(__dirname,'../src/client/index.js'));
+// });
+
+
+// express.static(path.resolve(__dirname,'../public')));
+// app.use('/static', express.static(path.resolve(__dirname, 'public'))); //doesnt work
+// app.use(express.static(path))
+
+// app.get('/', (req, res) => {
+//   res.status(200).send("hello world");
+//   // express.static(path.resolve(__dirname, '../index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`)
---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+console.log('hello from index.js')
+
+
+// import React from 'react';
+// import reactDom from "react-dom";
+// import App from "../client/components/App"
+// reactDom.render(<App/>, document.getElementById("root"));
+
